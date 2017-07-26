92ccfb9b0b6ff4059a9ded51ecb1a70e2dde3248
James <gotnospirit@gmail.com>
2017-07-26 17:58:11 +0200

    svg4everybody++

--- src/Artwork.js
+++ src/Artwork.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
 import './Artwork.css'
 
-export default () => (
-  <div id="artwork">
-    <div>
-      <svg role="img" viewBox="0 0 437 435">
-        <use xlinkHref="degrade.svg#artwork"/>
-      </svg>
+const svg4everybody = () => {
+  window.svg4everybody()
+}
+
+export default class extends Component {
+  componentDidMount() {
+    window.addEventListener('load', svg4everybody);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('load', svg4everybody);
+  }
+
+  render() {
+    return <div id="artwork">
+      <Helmet script={[ { 'src' : 'scripts/svg4everybody.min.js'} ]}/>
+      <div>
+        <svg role="img" viewBox="0 0 437 435">
+          <use xlinkHref="degrade.svg#artwork"/>
+        </svg>
+      </div>
     </div>
-  </div>
-)
+  }
+}
