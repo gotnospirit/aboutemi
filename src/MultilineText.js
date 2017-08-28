ef06fefcab3dd1fecb88edb0b94ee771c376f827
James <gotnospirit@gmail.com>
2017-08-28 14:43:47 +0200

    coding standard

--- src/MultilineText.js
+++ src/MultilineText.js
@@ -3,42 +3,36 @@ import ExternalLink from './ExternalLink'
 import EmailLink from './EmailLink'
 
 const renderText = (value, urls) => {
-  if (!urls)
-  {
+  if (!urls) {
     return value
   }
 
   let pattern = []
-  for (let key in urls)
-  {
+  for (let key in urls) {
     pattern.push('\\b' + key + '\\b')
   }
 
-  let re = new RegExp('(' + pattern.join('|') + ')', 'g')
+  const re = new RegExp('(' + pattern.join('|') + ')', 'g')
   let parts = value.split(re)
 
-  for (let i = 0, max = parts.length; i < max; ++i)
-  {
-    let label = parts[i]
-    let href = urls[label]
-    if (undefined !== href)
-    {
+  for (let i = 0, max = parts.length; i < max; ++i) {
+    const label = parts[i]
+    const href = urls[label]
+
+    if (undefined !== href) {
       parts[i] = (-1 === href.indexOf('@'))
-          ? <ExternalLink key={'external-link-' + i} href={href} label={label} />
-          : <EmailLink key={'link-' + i} address={href} label={label} />
+        ? <ExternalLink key={'external-link-' + i} href={href} label={label} />
+        : <EmailLink key={'link-' + i} address={href} label={label} />
     }
   }
   return parts
 }
 
 const renderDescription = (value, urls) => {
-  if ('[object Array]' !== Object.prototype.toString.call(value))
-  {
+  if ('[object Array]' !== Object.prototype.toString.call(value)) {
     value = [value]
   }
-  return value.map((v, index) => {
-    return (<span key={index}>{renderText(v, urls)}</span>);
-  })
+  return value.map((v, index) => <span key={index}>{renderText(v, urls)}</span>)
 }
 
 export default class extends Component {
