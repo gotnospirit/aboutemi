dabaf7720305bc492c9febe5606315aecc04aac7
James <gotnospirit@gmail.com>
2017-10-20 15:18:46 +0200

    react-ga++

--- src/App.js
+++ src/App.js
@@ -13,30 +13,39 @@ import NotFound from './NotFound'
 import { basename } from './data'
 import { getPageTitle } from './utils'
 import Router from './AnimationRouter'
+import withTracker from './GoogleAnalytics'
 import './App.css'
+
+const Screen = ({ basename, action, pathname }) => {
+  return (<Camera action={action} pathname={pathname}>
+    <Helmet
+      base={{ href: basename }}
+      title={getPageTitle()}
+      meta={[{
+        property: 'og:title',
+        content: getPageTitle()
+      }]}/>
+    <Polyfill/>
+    <Header is_homepage={'/' === pathname}/>
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/works" component={Works} />
+      <Route path="/works/:id" component={WorkComponent} />
+      <Route component={NotFound} status={404} />
+    </Switch>
+    <Footer/>
+  </Camera>)
+}
 
 export default () => (
   <Router basename={basename}>
     <Route render={({ history, location }) => (
-      <Camera action={history.action} pathname={location.pathname}>
-        <Helmet
-          base={{ href: basename }}
-          title={getPageTitle()}
-          meta={[{
-            property: 'og:title',
-            content: getPageTitle()
-          }]}/>
-        <Polyfill/>
-        <Header is_homepage={'/' === location.pathname}/>
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/works" component={Works} />
-          <Route path="/works/:id" component={WorkComponent} />
-          <Route component={NotFound} status={404} />
-        </Switch>
-        <Footer/>
-      </Camera>
+      <Route component={withTracker(Screen, {}, {
+        basename,
+        action: history.action,
+        pathname: location.pathname
+      })} />
     )} />
   </Router>
 )
---
+++ src/GoogleAnalytics.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import GoogleAnalytics from 'react-ga';
+
+GoogleAnalytics.initialize('UA-11395496-2');
+
+const withTracker = (WrappedComponent, options = {}, properties = {}) => {
+  const trackPage = page => {
+    GoogleAnalytics.set({
+      page,
+      ...options,
+    });
+    GoogleAnalytics.pageview(page);
+  };
+
+  const HOC = class extends Component {
+    componentDidMount() {
+      const page = this.props.location.pathname;
+      trackPage(page);
+    }
+
+    componentWillReceiveProps(nextProps) {
+      const currentPage = this.props.location.pathname;
+      const nextPage = nextProps.location.pathname;
+
+      if (currentPage !== nextPage) {
+        trackPage(nextPage);
+      }
+    }
+
+    render() {
+      console.log('options', options);
+      return <WrappedComponent {...this.props} {...properties} />;
+    }
+  };
+
+  return HOC;
+};
+
+export default withTracker;
